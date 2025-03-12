5860b1374cdbdbf107736040baa825cb8839db98
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-03-12 19:55:38 +0000

    chore: refactor css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,10 @@ import { RouterOutlet } from '@angular/router';
   imports: [FormsModule, PanelModule, RouterOutlet],
   template: `
     <main class="main">
-          <router-outlet></router-outlet>
+      <router-outlet></router-outlet>
     </main>
   `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  names: Array<string> = []
-
-  insert(name: string) {
-    this.names.push(name)
-  }
-
-  remove(item: string) {
-    this.names = this.names.filter(internalName => internalName !== item)
-  }
 }
