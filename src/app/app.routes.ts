c2c1d41b52a0f16acda22d397949e21b3676e7ee
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-03-15 23:24:14 -0300

    feat: new routes and menuitem for Credit, Debit, and Equity types of Category and Account

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,31 +8,74 @@ import { BeanComponent } from './bean.component';
 import { CategoryListComponent } from './category/category-list.component';
 import { CategoryInsertComponent } from './category/category-insert.component';
 import { CategoryUpdateComponent } from './category/category-update.component';
-import { CreditAccountListComponent } from './creditAccount/creditAccount-list.component';
-import { CreditAccountInsertComponent } from './creditAccount/creditAccount-insert.component';
-import { CreditAccountUpdateComponent } from './creditAccount/creditAccount-update.component';
+import { AccountListComponent } from './account/account-list.component';
+import { AccountInsertComponent } from './account/account-insert.component';
+import { AccountUpdateComponent } from './account/account-update.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'creditAccounts', component: BeanComponent, canActivate: [AuthGuard],
+  {
+    path: 'creditAccounts', component: BeanComponent, canActivate: [AuthGuard],
     data: { title: 'Credit Accounts' },
     children: [
-      { path: 'list', component: CreditAccountListComponent },
-      { path: 'new', component: CreditAccountInsertComponent },
-      { path: 'edit', component: CreditAccountUpdateComponent },
+      { path: 'list', component: AccountListComponent, data: { type: 'Credit' } },
+      { path: 'new', component: AccountInsertComponent, data: { type: 'Credit' } },
+      { path: 'edit', component: AccountUpdateComponent, data: { type: 'Credit' } },
       { path: '', redirectTo: 'list', pathMatch: 'full' }
     ]
   },
-  { path: 'categories', component: BeanComponent, canActivate: [AuthGuard],
-      data: { title: 'Categories' },
-      children: [
-        { path: 'list', component: CategoryListComponent },
-        { path: 'new', component: CategoryInsertComponent },
-        { path: 'edit', component: CategoryUpdateComponent },
-        { path: '', redirectTo: 'list', pathMatch: 'full' }
-      ]
+  {
+    path: 'debitAccounts', component: BeanComponent, canActivate: [AuthGuard],
+    data: { title: 'Debit Accounts' },
+    children: [
+      { path: 'list', component: AccountListComponent, data: { type: 'Debit' } },
+      { path: 'new', component: AccountInsertComponent, data: { type: 'Debit' } },
+      { path: 'edit', component: AccountUpdateComponent, data: { type: 'Debit' } },
+      { path: '', redirectTo: 'list', pathMatch: 'full' }
+    ]
   },
-  { path: 'owners', component: BeanComponent, canActivate: [AuthGuard],
+  {
+    path: 'equityAccounts', component: BeanComponent, canActivate: [AuthGuard],
+    data: { title: 'Equity Accounts' },
+    children: [
+      { path: 'list', component: AccountListComponent, data: { type: 'Equity' } },
+      { path: 'new', component: AccountInsertComponent, data: { type: 'Equity' } },
+      { path: 'edit', component: AccountUpdateComponent, data: { type: 'Equity' } },
+      { path: '', redirectTo: 'list', pathMatch: 'full' }
+    ]
+  },
+  {
+    path: 'creditCategories', component: BeanComponent, canActivate: [AuthGuard],
+    data: { title: 'Credit Categories' },
+    children: [
+      { path: 'list', component: CategoryListComponent, data: { type: 'Credit' } },
+      { path: 'new', component: CategoryInsertComponent, data: { type: 'Credit' } },
+      { path: 'edit', component: CategoryUpdateComponent, data: { type: 'Credit' } },
+      { path: '', redirectTo: 'list', pathMatch: 'full' }
+    ]
+  },
+  {
+    path: 'debitCategories', component: BeanComponent, canActivate: [AuthGuard],
+    data: { title: 'Debit Categories' },
+    children: [
+      { path: 'list', component: CategoryListComponent, data: { type: 'Debit' } },
+      { path: 'new', component: CategoryInsertComponent, data: { type: 'Debit' } },
+      { path: 'edit', component: CategoryUpdateComponent, data: { type: 'Debit' } },
+      { path: '', redirectTo: 'list', pathMatch: 'full' }
+    ]
+  },
+  {
+    path: 'equityCategories', component: BeanComponent, canActivate: [AuthGuard],
+    data: { title: 'Equity Categories' },
+    children: [
+      { path: 'list', component: CategoryListComponent, data: { type: 'Equity' } },
+      { path: 'new', component: CategoryInsertComponent, data: { type: 'Equity' } },
+      { path: 'edit', component: CategoryUpdateComponent, data: { type: 'Equity' } },
+      { path: '', redirectTo: 'list', pathMatch: 'full' }
+    ]
+  },
+  {
+    path: 'owners', component: BeanComponent, canActivate: [AuthGuard],
     data: { title: 'Owners' },
     children: [
       { path: 'list', component: OwnerListComponent },
@@ -41,6 +84,6 @@ export const routes: Routes = [
       { path: '', redirectTo: 'list', pathMatch: 'full' }
     ]
   },
-  { path: '', redirectTo: 'login', pathMatch: 'full'},
-  { path: '**', redirectTo: 'login'}
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' }
 ];
