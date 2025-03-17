583e670229aebf504ee7b960e11cf3122855e595
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-03-17 00:01:51 -0300

    feat: create entry and entryInsertUpdate models

---
+++ src/app/entry/entry.ts
@@ -0,0 +1,16 @@
+import { Account } from "../account/account";
+import { Bean } from "../bean/bean";
+
+export class Entry implements Bean<number> {
+  constructor(public id: number, public inOwner: string, public outOwner: string, public date: Date, public inAccount: Account,
+    public outAccount: Account, public value: number, public note: string) { }
+
+  getId(): number {
+    return this.id
+  }
+}
+
+export class EntryInsertUpdate {
+  constructor(public inOwner: string, public outOwner: string, public date: Date, public inAccount: Account,
+    public outAccount: Account, public value: number, public note: string) { }
+}
