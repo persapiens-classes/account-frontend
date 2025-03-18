9bd716a261ec2d1ca716a21a0f2effc7992c7132
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-03-18 20:31:59 +0000

    feat: change default logged page to Credit Owner EquityAccount Initial Value

--- src/app/auth/login.component.ts
+++ src/app/auth/login.component.ts
@@ -70,7 +70,7 @@ export class LoginComponent {
     if (this.loginForm.valid) {
       this.authService.signin(this.loginForm.value.inputUsername, this.loginForm.value.inputPassword).pipe(
         tap((loginResponse) => {
-          this.router.navigate(['creditEntries/list'])
+          this.router.navigate(['ownerEquityAccountInitialValues/list'])
         }),
         catchError((error) => {
           this.messageService.add({
