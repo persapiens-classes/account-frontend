c00f0f10f8109dd49b16b53f1dadea738d06abf9
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-07-05 21:09:16 +0000

    chore:  remove toBean BeanDetailComponent method

--- src/app/bean/bean-detail.component.ts
+++ src/app/bean/bean-detail.component.ts
@@ -3,14 +3,7 @@ import { Bean, toBean } from './bean';
 export class BeanDetailComponent<T extends Bean> {
   bean: T;
 
-  constructor(
-    private readonly beanCreateFunction: () => T,
-    private readonly jsonToBeanFunction: (t: T) => T,
-  ) {
-    this.bean = this.toBean(history.state.bean);
-  }
-
-  toBean(json: unknown): T {
-    return toBean(json, this.beanCreateFunction, this.jsonToBeanFunction);
+  constructor(beanCreateFunction: () => T, jsonToBeanFunction: (t: T) => T) {
+    this.bean = toBean(history.state.bean, beanCreateFunction, jsonToBeanFunction);
   }
 }
