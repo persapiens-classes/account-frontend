ef54ddf1db639f1ce6e3b23062f71cc888f2b6b7
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-07-14 11:25:37 +0000

    chore: remove bean detail component

--- src/app/bean/bean-detail.component.ts
+++
@@ -1,9 +0,0 @@
-import { Bean, toBean } from './bean';
-
-export class BeanDetailComponent<T extends Bean> {
-  bean: T;
-
-  constructor(beanCreateFunction: () => T, jsonToBeanFunction: (t: T) => T) {
-    this.bean = toBean(history.state.bean, beanCreateFunction, jsonToBeanFunction);
-  }
-}
