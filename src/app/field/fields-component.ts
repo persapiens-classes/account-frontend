1cc2d07d4741da39838b28a40a4806427c8fe301
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-11-07 07:59:28 -0300

    chore: refine field component interfaces

--- src/app/field/fields-component.ts
+++ src/app/field/fields-component.ts
@@ -1,39 +1,39 @@
 import { InputSignal } from '@angular/core';
+import { FieldTree } from '@angular/forms/signals';
 
 /**
  * Base interface for all field components
  */
-export interface FieldComponent {
-  isDisabled?: boolean;
+export interface FieldComponent<T = unknown> {
   label?: InputSignal<string>;
   id?: InputSignal<string>;
   autoFocus?: InputSignal<boolean>;
-  setDisabledState?: (isDisabled: boolean) => void;
+  field: InputSignal<FieldTree<T>>;
 }
 
 /**
  * Interface for date field components
  */
-export interface DateFieldComponent extends FieldComponent {
+export interface DateFieldComponent extends FieldComponent<Date | null> {
   showIcon?: InputSignal<boolean>;
 }
 
 /**
  * Type for string/text input field components
  */
-export type StringFieldComponent = FieldComponent;
+export type StringFieldComponent = FieldComponent<string>;
 
 /**
  * Type for number input field components
  */
-export type NumberFieldComponent = FieldComponent;
+export type NumberFieldComponent = FieldComponent<number | null>;
 
 /**
  * Interface for select field components
  */
-export interface SelectFieldComponent<T = unknown> extends FieldComponent {
+export interface SelectFieldComponent<T = unknown> extends FieldComponent <T | null>{
   options: InputSignal<T[]>;
   optionLabel: InputSignal<string>;
-  optionValue: InputSignal<string>;
+  optionValue: InputSignal<string>; 
   placeholder: InputSignal<string>;
 }
