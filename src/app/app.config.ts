8a8418467e521f16babfb94537fc9e8b54fdaa1f
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-12-06 11:33:05 +0000

    chore: updating angular, primeng, vitest. provideAnimationsAsync  is safe to remove with angular v21.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,9 +8,6 @@ import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-// provideAnimationsAsync used by primeng components that require animations
-// eslint-disable-next-line sonarjs/deprecation
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { authIntercept } from './auth/auth.interceptor';
 import { MessageService } from 'primeng/api';
 
@@ -20,8 +17,6 @@ export const appConfig: ApplicationConfig = {
     MessageService,
     provideZonelessChangeDetection(),
     provideRouter(routes),
-    // eslint-disable-next-line @typescript-eslint/no-deprecated, sonarjs/deprecation
-    provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Aura,
